90e6157f115f4ac7899ac75a019469d5678be72a
Bell John Demetria <vetgamers14@gmail.com>
2022-10-03 13:28:47 -0700

    Test Site 1st Commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 
 import App from './App';
 import "./index.css"
 
+// 👇️ IMPORTANT: use correct ID of your root element
+// this is the ID of the div in your index.html file
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
+// 👇️ if you use TypeScript, add non-null (!) assertion operator
+// const root = createRoot(rootElement!);
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
+
