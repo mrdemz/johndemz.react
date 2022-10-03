af24281e7df94f0140429bdc89a508ea3dca2ce7
Bell John Demetria <vetgamers14@gmail.com>
2022-10-03 13:00:53 -0700

    Test Site 1st Commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 
-import {createRoot} from 'react-dom/client';
+
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom'
 
 import App from './App';
 import "./index.css"
 
-// 👇️ IMPORTANT: use correct ID of your root element
-// this is the ID of the div in your index.html file
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-// 👇️ if you use TypeScript, add non-null (!) assertion operator
-// const root = createRoot(rootElement!);
 
 
 
