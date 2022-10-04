c3221772a7300ac3de9ea8ee538aaeadb8751def
Bell John Demetria <vetgamers14@gmail.com>
2022-10-03 21:35:24 -0700

    Test Site 1st Commit

--- src/NaviBar.jsx
+++ src/NaviBar.jsx
@@ -45,7 +45,8 @@ function NaviBar() {
     </Container>
   </Navbar>
   
-      <Routes>    
+      <Routes> 
+        <Route index element={<Home/>}/>   
        <Route path="/" element={<Home/>}></Route>
        <Route path="about" element={<About/>}> </Route>
         <Route   path="skills" element={<Skills/>}> </Route>
